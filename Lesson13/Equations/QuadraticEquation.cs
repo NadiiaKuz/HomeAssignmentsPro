cc21b011ea57222b0d4caf8a4454b3c862879d74
Nadiia <nadink1603@gmail.com>
2023-10-12 16:53:39 +0200

    Added classes; WhenAny to WaitAny

---
+++ Lesson13/Equations/QuadraticEquation.cs
@@ -0,0 +1,28 @@
+﻿namespace Lesson13.Equations
+{
+    static class QuadraticEquation
+    {
+        public static string CalculateQuadraticEquation(double a, double b, double c)
+        {
+            Random random = new();
+            Task.Delay(random.Next(10, 1000)).Wait();
+
+            double discriminant = Math.Pow(b, 2) - 4 * a * c;
+
+            if (discriminant > 0)
+            {
+                double x1 = (-b + Math.Sqrt(discriminant)) / 2 * a;
+                double x2 = (-b - Math.Sqrt(discriminant)) / 2 * a;
+                return $"x1={x1} and x2={x2}";
+            }
+
+            if (discriminant == 0)
+            {
+                double x = -b / 2 * a;
+                return $"x={x}";
+            }
+
+            return "x not found";
+        }
+    }
+}
--- Lesson13/Program.cs
+++ Lesson13/Program.cs
@@ -1,11 +1,13 @@
-﻿namespace Lesson13
+﻿using Lesson13.Equations;
+
+namespace Lesson13
 {
     internal class Program
     {
-        private static readonly Random random = new();
-
         static void Main(string[] args)
         {
+            Random random = new();
+
             double a = 1;
 
             for (int j = 0; j < 50; j++)
@@ -13,12 +15,12 @@
                 double b = random.Next(-20, 20);
                 double c = random.Next(-20, 20);
 
-                Task<string> task1 = Task.Run(() => CalculateQuadraticEquation(a, b, c));
-                Task<string> task2 = Task.Run(() => CalculateQuadraticEquationVieta(a, b, c));
+                Task<string> task1 = Task.Run(() => QuadraticEquation.CalculateQuadraticEquation(a, b, c));
+                Task<string> task2 = Task.Run(() => QuadraticEquationVieta.CalculateQuadraticEquationVieta(a, b, c));
 
-                Task.WhenAny(task1, task2).Wait();
+                var taskCompletedId = Task.WaitAny(task1, task2);
 
-                if (task1.IsCompleted)
+                if (taskCompletedId == 0)
                 {
                     Console.WriteLine("Standard method: ");
                     Console.WriteLine(task1.Result);
@@ -32,55 +34,5 @@
                 Console.WriteLine();
             }
         }
-
-        static string CalculateQuadraticEquation(double a, double b, double c)
-        {
-            WaitRandomTime();
-
-            double discriminant = Math.Pow(b, 2) - 4 * a * c;
-
-            if (discriminant > 0)
-            {
-                double x1 = (-b + Math.Sqrt(discriminant)) / 2 * a;
-                double x2 = (-b - Math.Sqrt(discriminant)) / 2 * a;
-                return $"x1={x1} and x2={x2}";
-            }
-
-            if (discriminant == 0)
-            {
-                double x = -b / 2 * a;
-                return $"x={x}";
-            }
-
-            return "x not found";
-        }
-
-        static string CalculateQuadraticEquationVieta(double a, double b, double c)
-        {
-            WaitRandomTime();
-
-            double sumOfRoots = -b / a;
-            double productOfRoots = c / a;
-
-            if (productOfRoots > 0)
-            {
-                double sqrtProduct = Math.Sqrt(productOfRoots);
-
-                double x1 = (sumOfRoots + sqrtProduct);
-                double x2 = (sumOfRoots - sqrtProduct);
-                return $"x1={x1} and x2={x2}";
-            }
-            
-            if (productOfRoots == 0)
-            {
-                double x = sumOfRoots;
-                return $"x={x}";
-            }
-
-            return "x not found";
-        }
-
-        static void WaitRandomTime() =>
-            Task.Delay(random.Next(10, 1000)).Wait();
     }
 }

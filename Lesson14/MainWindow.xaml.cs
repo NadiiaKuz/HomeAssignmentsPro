2e8917c042e48c72ce791d27db81e3a49edb2359
Nadiia <nadink1603@gmail.com>
2023-10-16 14:06:17 +0200

    Added Lesson14

---
+++ Lesson14/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Lesson14
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly string _path = "..//..//..//..//..//Where_the_crawdads_sing.txt";
+
+        private CancellationTokenSource _source;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void ButtonLoad_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox.Text = "Text is loading...";
+
+            _source = new CancellationTokenSource();
+            CancellationToken token = _source.Token;
+
+            try
+            {
+                var content = await File.ReadAllTextAsync(_path, token);
+                TextBox.Text = content[..100000];
+            }
+            catch (Exception ex)
+            {
+                if (ex is TaskCanceledException)
+                    TextBox.Text = "Load was canceled";
+                else
+                    TextBox.Text = ex.Message;
+            }
+        }
+
+        private void ButtonCancel_Click(object sender, RoutedEventArgs e) =>
+            _source?.Cancel();
+    }
+}
